df47063b0d2540c3f29f249ba9a7aeae4d43b25c
Robin Jeffries <robin.c.jeffries@gmail.com>
2025-05-05 00:00:39 +0000

    fixed infer script

--- tts_model_infer.py
+++ tts_model_infer.py
@@ -1,23 +1,30 @@
 from TTS.api import TTS
+from torch.cuda import is_available
+from pathlib import Path
+
+# from Trainer
+model_name = "tts_models/en/ljspeech/glow-tts"
+model_name = "tts_models/en/ljspeech/tacotron2-DDC"
+model_name = "tts_models/en/vctk/vits"
+model_name = "tts_models/multilingual/multi-dataset/your_tts"
+model_name = "tts_models/multi-dataset/xtts_v2"
 
 print("Loaded TTS")
-# print(TTS.list_models())
-tts = TTS("tts_models/multi-dataset/xtts_v2", gpu=False)
+print(TTS.list_models())
+# tts = TTS("tts_models/multi-dataset/xtts_v2", gpu=is_available())
+# tts = TTS("tts_models/en/ljspeech/tacotron2-DCA", gpu=is_available())
+tts = TTS(model_name, gpu=is_available())
 # tts.model
 import time
 
+first_speaker = tts.speakers[0] if tts.speakers else None
+# exit()
 start = time.time()
 tts.tts_to_file(
     text="It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent.",
     file_path="output.wav",
-    # speaker="Brenda Stern",
-    speaker_wav=[
-        "./geralt/0x00112c9e.wav",
-        "./geralt/0x001133d2.wav",
-        "./geralt/0x001136dd.wav",
-        "./geralt/0x0011351f.wav",
-        "./geralt/0x0011fc7c.wav",
-    ],
+    # speaker=first_speaker,
+    speaker_wav=list(Path("custom/geralt").glob("*.wav")),
     language="en",
     split_sentences=True,
 )
